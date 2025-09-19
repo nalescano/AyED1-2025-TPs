3b3b36f13d9d7437d4e938191d7f0bc6cc5c8c9f
nalescano <nataliajosefinalescano@gmail.com>
2025-09-19 04:02:25 -0300

    arreglo de contrato

--- Tp2_listas/3.py
+++ Tp2_listas/3.py
@@ -1,13 +1,29 @@
 
+def lista_cuadrados(n: int) -> list:
+    '''
+    crea una lista vacia y recorre desde 1 al  n (entero que ingresa el usuario) y agrega a una nueva lista el primer numero al cuadrado.
 
+    Pre:
+        - ingresa un entero (n)
 
-def lista_cuadrados(n):
+    Post:   
+        - devuelve una lista con los numeros al cuadrado (cuadrados).
+    '''
+
     cuadrados = []
     for i in range(1, n+1):
         cuadrados.append(i * 2)
     return cuadrados
 
-def main():
+def main() -> None:
+    '''
+    funcion principal.
+
+    pre:
+        - no ingresa ningun valor como parametro.
+    post:
+        - llama a las funciones principales.
+    '''
     n = int(input('ingrese un numero: '))
     resultados = lista_cuadrados(n)
     
